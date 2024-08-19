f0dd186a3966a3676c26d2afa986ec27ebd3354e
Laboratorio C205 <fergomez@uamv.edu.ni>
2024-08-19 08:31:15 -0600

    first commit

---
+++ HolaJuan/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HolaJuan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi nombre es Fernando Gomez");
+            Console.WriteLine("Tengo la edad de 18");
+            Console.Write("Soy de Granada");
+
+        }
+    }
+}
